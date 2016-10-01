6133a81afe3c2a3744b522b0754f8c807e66b02c
Shaika-Dzari <shaikadzari@4nakama.net>
2016-10-01 18:25:26 -0400

    some work

--- app/server/message/routes.js
+++ app/server/message/routes.js
@@ -3,15 +3,16 @@ var router = express.Router();
 var passport = require('passport');
 var message = require('./message');
 
+
 // GET messages (blog post)
-router.get('/', function(req, res, next) {
+router.get('/public/', function(req, res, next) {
     message.find({published: 1}, '-authorId', function (err, mgs) {
         if (err) next(err);
         res.json(messages);
     });
 });
 
-router.get('/:messageid', function(req, res, next) {
+router.get('/public/:messageid', function(req, res, next) {
     var id = req.params.messageid;
 
     message.findOne({published: 1, _id: id}, '-authorId', function (err, mgs) {
@@ -19,6 +20,44 @@ router.get('/:messageid', function(req, res, next) {
         res.json(messages);
     });
 });
+
+router.get('/protected/', passport.authenticate('local'), function(req, res, next) {
+    message.find({}, '-authorId', function (err, mgs) {
+        if (err) next(err);
+        res.json(messages);
+    });
+});
+
+router.get('/protected/:messageid', passport.authenticate('local'), function(req, res, next) {
+    var id = req.params.messageid;
+
+    message.findOne({_id: id}, '-authorId', function (err, mgs) {
+        if (err) next(err);
+        res.json(messages);
+    });
+});
+
+/**
+ * Create new message.
+ */
+router.post('/protected/', passport.authenticate('local'), function(req, res, next) {
+
+    var msg = req.body;
+    req.status(201);
+});
+
+
+/**
+ * Update new message.
+ */
+router.put('/protected/:messageid', passport.authenticate('local'), function(req, res, next) {
+    var id = req.params.messageid;
+    var msg = req.body;
+
+    req.status(201);
+});
+
+
 
 router.post('/', function(req, res) {
     var cat = req.body;
--- app/ui/page/blog/blogpage.jsx
+++ app/ui/page/blog/blogpage.jsx
@@ -13,14 +13,13 @@ export default class BlogPage extends React.Component {
 
     componentWillMount() {
 
-            var self = this;
-            window.fetch('/api/tags')
-                    .then(r => r.json())
-                    .then(data => {
-                        self.setState({tags: data});
-                    })
-                    .catch(e => console.log("Booo" + e));
-
+        var self = this;
+        window.fetch('/api/tags')
+                .then(r => r.json())
+                .then(data => {
+                    self.setState({tags: data});
+                })
+                .catch(e => self.setState({error: e}));
     }
 
 
@@ -28,16 +27,33 @@ export default class BlogPage extends React.Component {
         var t = null;
 
         if (this.state.tags) {
-            var t = this.state.tags.map((v, idx) => {
+            t = this.state.tags.map((v, idx) => {
                 return <a className="tag" href="#" key={v._id}>{v.name}</a>
             });
+
+        } else if (this.state.error) {
+            t = <AlertBox message={this.state.error} />
         }
 
         return (
-            <div>
-                <h1>Blog Page</h1>
-                <div>
-                    {t}
+            <div className="row">
+                <div className="col-10">
+                    <h1>Blog Page</h1>
+                    <div>
+
+                        ...
+                    </div>
+                </div>
+                <div className="col-2">
+                    <div>
+                        <h3>Catégories</h3>
+
+                        ....
+                    </div>
+                    <div>
+                        <h3>Tags</h3>
+                        {t}
+                    </div>
                 </div>
             </div>
         );
