bd88023f6b55d94b231a00284c235727601e55c2
Shaika-Dzari <shaikadzari@4nakama.net>
2016-12-14 08:16:37 -0500

    Module tables

--- app/server/config/schema-postgresql.sql
+++ app/server/config/schema-postgresql.sql
@@ -1,4 +1,4 @@
-create table nakama.account (
+create table account (
     id serial primary key,
     username text not null,
     password text not null,
@@ -10,14 +10,79 @@ create table nakama.account (
     unique (username)
 );
 
-create table nakama.category (
+-- Default / disabled account
+insert into account(username, password, displayname, email, role, enabled)
+values('admin@system', '-', 'System', 'admin@system', 'author', false);
+
+create table module (
+    id serial primary key,
+    code text not null,
+    name text not null,
+    enablemodule boolean not null default true,
+    enablecomment boolean not null default true,
+    enablecategory boolean not null default true,
+    createdat timestamp with time zone not null default now(),
+    unique (name)
+);
+
+-- Basic Modules
+insert into module(code, name, enablecomment, enablecategory) values('BLOG', 'Blog', true, true);
+insert into module(code, name, enablecomment, enablecategory) values('STORY', 'Stories', true, true);
+insert into module(code, name, enablecomment, enablecategory) values('PROJECT', 'Projects', true, false);
+insert into module(code, name, enablecomment, enablecategory) values('ABOUT', 'About', false, false);
+
+create table modulemetatype (
     id serial primary key,
     name text not null,
     createdat timestamp with time zone not null default now(),
     unique (name)
 );
 
-create table nakama.message (
+-- Some common type
+insert into modulemetatype(name) values('text');
+insert into modulemetatype(name) values('url');
+insert into modulemetatype(name) values('file');
+insert into modulemetatype(name) values('date');
+
+create table modulemeta (
+    id serial primary key,
+    key text not null,
+    value text not null,
+    valuetypeid integer not null,
+    moduleid integer not null,
+    createdat timestamp with time zone not null default now(),
+    unique (key, moduleid),
+    constraint fk_modmeta_moduleid foreign key (moduleid) references module (id),
+    constraint fk_modmeta_vtid foreign key (valuetypeid) references modulemetatype (id)
+);
+
+create index idx_modmeta_moduleid on modulemeta(moduleid);
+
+create table category (
+    id serial primary key,
+    name text not null,
+    moduleid integer not null,
+    createdat timestamp with time zone not null default now(),
+    unique (name, moduleid),
+    constraint fk_modmeta_moduleid foreign key (moduleid) references module (id)
+);
+
+create index idx_cat_moduleid on category(moduleid);
+
+-- Default Categories
+with module_blog as ( select * from module where code = 'BLOG' ),
+cats as (
+    select 'Development' as name union all
+    select 'Movie' union all
+    select 'Book' union all
+    select 'Blog'
+)
+insert into category(name, moduleid)
+select c.name, m.id
+from cats c, module_blog m
+;
+
+create table message (
     id serial primary key,
     title text not null,
     body text not null,
@@ -26,16 +91,26 @@ create table nakama.message (
     authorname text not null,
     authorid integer,
     prettyurl text not null,
+    moduleid integer not null,
+    categories jsonb,
     createdat timestamp with time zone not null default now(),
     updatedat timestamp with time zone,
-    messagetype text not null default 'MESSAGE',
-    categories jsonb,
-    constraint fk_message_accountid foreign key (authorid) references account (id)
+    constraint fk_message_accountid foreign key (authorid) references account (id),
+    constraint fk_message_moduleid foreign key (moduleid) references module (id)
 );
 
-create index on nakama.message(createdat);
+create index idx_message_createdat on message(createdat);
 
-create table nakama.comment (
+-- Welcome message
+with
+module_blog as ( select * from module where code = 'BLOG' ),
+default_account as ( select * from account where username = 'admin@system' )
+insert into message(title, body, bodyhtml, published, authorname, authorid, prettyurl, moduleid)
+select 'Welcome', 'Welcome to your blog!', '<p>Welcome to your blog!</p>', true, a.displayname, a.id, '/welcome', m.id
+from module_blog m, default_account a;
+
+
+create table comment (
     id serial primary key,
     body text not null,
     approved boolean not null default TRUE,
@@ -48,9 +123,9 @@ create table nakama.comment (
     constraint fk_message_accountid foreign key (authorid) references account (id)
 );
 
-create index on nakama.comment(messageid);
+create index idx_comment_msgid on comment(messageid);
 
-create table nakama.file (
+create table file (
     id serial primary key,
     name text not null,
     filepath text not null,
@@ -63,7 +138,7 @@ create table nakama.file (
 );
 
 
-create table nakama.statistics (
+create table statistics (
     id serial primary key,
     tablename text not null,
     statistic text not null,
@@ -74,9 +149,9 @@ create table nakama.statistics (
 );
 
 -- Basic stats
-insert into nakama.statistics(tablename, statistic, value) values('file', 'total_count', '0');
-insert into nakama.statistics(tablename, statistic, value) values('message', 'total_count', '0');
-insert into nakama.statistics(tablename, statistic, value) values('comment', 'total_count', '0');
+insert into statistics(tablename, statistic, value) values('file', 'total_count', '0');
+insert into statistics(tablename, statistic, value) values('message', 'total_count', '0');
+insert into statistics(tablename, statistic, value) values('comment', 'total_count', '0');
 
 -- Procedure to calculate stats
 create or replace function calculate_statistics_message() returns trigger as $$
@@ -86,7 +161,8 @@ begin
 end;
 $$
 LANGUAGE plpgsql
-SET search_path = nakama;
+-- SET search_path = nakama
+;
 
 create or replace function calculate_statistics_file() returns trigger as $$
 begin
@@ -95,7 +171,8 @@ begin
 end;
 $$
 LANGUAGE plpgsql
-SET search_path = nakama;
+-- SET search_path = nakama
+;
 
 create or replace function calculate_statistics_comment() returns trigger as $$
 begin
@@ -104,14 +181,15 @@ begin
 end;
 $$
 LANGUAGE plpgsql
-SET search_path = nakama;
+-- SET search_path = nakama
+;
 
 -- Triggers to calculate statistics automatically
-create trigger trg_message_insert_stats after insert on nakama.message execute Procedure calculate_statistics_message();
-create trigger trg_message_delete_stats after delete on nakama.message execute Procedure calculate_statistics_message();
+create trigger trg_message_insert_stats after insert on message execute Procedure calculate_statistics_message();
+create trigger trg_message_delete_stats after delete on message execute Procedure calculate_statistics_message();
 
-create trigger trg_file_insert_stats after insert on nakama.file execute Procedure calculate_statistics_file();
-create trigger trg_file_delete_stats after delete on nakama.file execute Procedure calculate_statistics_file();
+create trigger trg_file_insert_stats after insert on file execute Procedure calculate_statistics_file();
+create trigger trg_file_delete_stats after delete on file execute Procedure calculate_statistics_file();
 
-create trigger trg_comment_insert_stats after insert on nakama.comment execute Procedure calculate_statistics_comment();
-create trigger trg_comment_delete_stats after delete on nakama.comment execute Procedure calculate_statistics_comment();
+create trigger trg_comment_insert_stats after insert on comment execute Procedure calculate_statistics_comment();
+create trigger trg_comment_delete_stats after delete on comment execute Procedure calculate_statistics_comment();
